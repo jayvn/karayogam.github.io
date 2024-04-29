8f80c6303581de1bb4ecab13016d2eb8dc6cdd9f
Jay Nair <jayakrishnan.nair@valustat.co>
2024-04-29 10:41:24 +0200

    Add mystery

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function revealMessage() {
-    document.getElementById('secretMessage').style.display = 'block';
+    var secretMessage = document.getElementById('secretMessage');
+    secretMessage.style.display = 'block'; // Show the secret message
+    setTimeout(function() {
+        document.body.classList.add('fade-out'); // Fade out the entire page
+    }, 1000); // Page starts to fade out 5 seconds after revealing the secret message
     return false; // Prevent default link behavior
 }
+
