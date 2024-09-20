3230cfe83b4ecac0847f9e6d4c227857aefec3aa
Jay Nair <jayakrishnan.nair@valustat.co>
2024-09-20 13:47:47 +0200

    Added events page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function revealMessage() {
     var secretMessage = document.getElementById('secretMessage');
     secretMessage.style.display = 'block'; // Show the secret message
     setTimeout(function() {
-        document.body.classList.add('fade-out'); // Fade out the entire page
+        secretMessage.classList.add('fade-out');
     }, 1000); // Page starts to fade out 5 seconds after revealing the secret message
     return false; // Prevent default link behavior
 }
